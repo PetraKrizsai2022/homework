a471e66fe04e5e12173a8163f57100e5d1008a18
PetraKrizsai2022 <krizsai@gmail.com>
2022-12-03 15:12:48 +0100

    unused import removed

--- src/test/java/tests/ui/BaseTest.java
+++ src/test/java/tests/ui/BaseTest.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.IOException;
-
 public abstract class BaseTest {
     protected WebDriver webDriver;
     protected final ObjectMapper objectMapper = new ObjectMapper();
